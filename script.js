205069f5c7cd70d4ea57feb1b168254d7fabebb3
heloisaszmgel <heloszmgel@gmail.com>
2025-04-02 14:40:18 -0400

    finished projectgit add .

--- script.js
+++ script.js
@@ -215,9 +215,12 @@ const renderSongs = (array) => {
     resetButton.addEventListener("click", () => {
       userData.songs = [...allSongs];
 
+      renderSongs(sortSongs()); 
+      setPlayButtonAccessibleText();
+      resetButton.remove();
     });
 
-  }
+  };
 
 };
 
@@ -248,6 +251,23 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;  
+
+ pauseSong()
+ setPlayerDisplay()
+ highlightCurrentSong()
+ setPlayButtonAccessibleText()
+    }
+});
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
@@ -265,3 +285,4 @@ const sortSongs = () => {
 };
 
 renderSongs(sortSongs());
+setPlayButtonAccessibleText();
